1009f5422282a2f800c2c10e84e807c150d4bcd4
Bcoder19 <bolivares2022@columbiasecondary.org>
2020-04-04 13:02:31 -0400

    Rock Paper Scissors XML done

--- Gamify/app/src/main/java/com/example/gamify/MainActivity.java
+++ Gamify/app/src/main/java/com/example/gamify/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gamify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRockPaperScissors(View view) {
+        Intent intent = new Intent(this,RockPaperScissorsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openTicTacToe(View view) {
+        Intent intent = new Intent(this,TicTacToeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openNim(View view) {
+        Intent intent = new Intent(this,NimActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Gamify/app/src/main/java/com/example/gamify/NimActivity.java
@@ -0,0 +1,14 @@
+package com.example.gamify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nim);
+    }
+}
